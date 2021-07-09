eb430483ac1da2eb2c5bda05347a5992bb0c68a0
Alija Fajić <62569376+alija98@users.noreply.github.com>
2021-07-09 17:09:15 +0200

    Updating responsive design

--- src/Components/Sidebar.js
+++ src/Components/Sidebar.js
@@ -1,18 +1,37 @@
-import React from "react";
+import React, { useRef, useEffect } from "react";
 import { Link } from "react-router-dom";
 import { useGlobalContext } from "../context";
 
 function Sidebar() {
-  const { sidebarShow } = useGlobalContext();
-  console.log(sidebarShow);
+  const { sidebarShow, changeSidebarShow } = useGlobalContext();
+  const node = useRef();
+  useEffect(() => {
+    console.log(sidebarShow);
+    if (sidebarShow) {
+      document.addEventListener("mousedown", handleClick);
+    } else {
+      return () => {
+        document.removeEventListener("mousedown", handleClick);
+      };
+    }
+  }, [sidebarShow]);
+  const handleClick = (e) => {
+    if (node.current.contains(e.target)) {
+      return;
+    }
+    changeSidebarShow();
+  };
   return (
-    <section className={`sidebar${sidebarShow ? "__show" : ""}`}>
+    <section
+      className={`${sidebarShow ? "sidebar show" : "sidebar"}`}
+      ref={node}
+    >
       <div className="sidebar__menu">
         <ul className="sidebar__list">
-          <Link to="/">
+          <Link to="/" onClick={() => changeSidebarShow()}>
             <li>SHOP</li>
           </Link>
-          <Link to="/about">
+          <Link to="/about" onClick={() => changeSidebarShow()}>
             <li>ABOUT US</li>
           </Link>
         </ul>
